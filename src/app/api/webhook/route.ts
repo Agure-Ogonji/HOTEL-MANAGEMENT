907c012c435e1184d91e758a503a85080a13cea5
Agure Ogonji <56344381+Agure-Ogonji@users.noreply.github.com>
2024-01-29 11:19:48 +0300

    Update route.ts

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -5,7 +5,7 @@ import Stripe from "stripe";
 const checkout_session_completed = "checkout.session.completed";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-    apiVersion: "2023-10-16",
+    apiVersion: "2023-08-16",
 });
 
 export async function POST(req: Request, res: Response){
