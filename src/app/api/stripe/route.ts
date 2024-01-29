1e464740ab12d25a3e5eb8f54324e24aec53c405
Agure Ogonji <56344381+Agure-Ogonji@users.noreply.github.com>
2024-01-29 11:19:09 +0300

    Update route.ts

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-    apiVersion: "2023-10-16",
+    apiVersion: "2023-08-16",
 });
 
 
